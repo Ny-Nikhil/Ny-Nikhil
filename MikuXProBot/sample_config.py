50130fca2c72072b850d0b0d334463ffef507c21
Ny-Nikhil <105223708+Ny-Nikhil@users.noreply.github.com>
2022-05-09 20:13:12 +0530

    Update sample_config.py

--- MikuXProBot/sample_config.py
+++ MikuXProBot/sample_config.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open('{}/MikuXProBot/{}'.format(os.getcwd(), config),
+    with open('{}/Ny-Nikhilot/{}'.format(os.getcwd(), config),
               'r') as json_file:
         return json.load(json_file)[key]
 
@@ -17,11 +17,11 @@ class Config(object):
     API_ID = "awoo"  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "awoo"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 769830161 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5033872287 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "awoo"
-    SUPPORT_CHAT = 'mikussupport'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -123456789  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -123446789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    SUPPORT_CHAT = 'Gianic bot support'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -1001534970564  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001534970564  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
