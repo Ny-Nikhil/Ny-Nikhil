4de0f4bc74240214ba6044f37f1f8bd0708c2c6e
Ny-Nikhil <105223708+Ny-Nikhil@users.noreply.github.com>
2022-05-10 07:48:05 +0530

    Update welcome_sql.py

--- MikuXProBot/modules/sql/welcome_sql.py
+++ MikuXProBot/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from MikuXProBot.modules.helper_funcs.msg_types import Types
-from MikuXProBot.modules.sql import BASE, SESSION
+from Ny-NikhilBot.modules.helper_funcs.msg_types import Types
+from Ny-NikhilBot.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
